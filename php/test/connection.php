730abb432b1ac42d83ec929b6109a378180040e5
anindita-code <anindita@uaguria.com>
2020-08-08 17:05:43 +0530

    Saving to Github

---
+++ php/test/connection.php
@@ -0,0 +1,17 @@
+<?php
+class Foo
+{
+    private $foo;
+    protected $bar;
+    public $baz;
+
+    public function __construct()
+    {
+        $this->foo = 1;
+        $this->bar = 2;
+        $this->baz = new StdClass;
+    }
+}
+
+var_dump( (array) new Foo );
+?>
